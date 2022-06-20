ca61c266982852a340824bb9e3ddfb8954ac27ea
LuizAlberto93 <costaesilva93@outlook.com>
2022-06-20 18:24:58 -0300

    subindo views atualizadas

--- Planner.Web/Models/AvaliacaoViewModel.cs
+++ Planner.Web/Models/AvaliacaoViewModel.cs
@@ -8,13 +8,16 @@ namespace Planner.Web.Models
     public class AvaliacaoViewModel : IAgendavelViewModel
     {
         [Key]
+        [Display(Name = "Cod.")]
         [Required]
         public int Id_Avaliacao { get; set; }
         [Required]
         public int Id_Materia { get; set; }
-        [Required(ErrorMessage ="O campo titulo é obrigatório")]
+        [Display(Name = "Avaliação")]
+        [Required(ErrorMessage ="Indentificação da Avaliação obrigatória")]
         public string Titulo { get; set; }
         public double? Nota { get; set; }
+        [Display(Name = "Dia/Horário")]
         public DateTime? Data_Hora { get; set; }
         public int Id_Evento { get; set; }
     }
