b087810628deccc1476b29511a661ef833017ca1
LuizAlberto93 <costaesilva93@outlook.com>
2022-06-22 15:15:07 -0300

    segunda modelagem de telas

--- Planner.Web/Models/MateriaViewModel.cs
+++ Planner.Web/Models/MateriaViewModel.cs
@@ -9,10 +9,18 @@ namespace Planner.Web.Models
         public int Id_Materia { get; set; }
         [Required(ErrorMessage = "O campo id_usuario é obrigatório.")]
         public int? Id_Usuario { get; set; }
+
+        [Display(Name = "Matéria")]
         public string Titulo { get; set; }
+
+        [Display(Name = "E-mail")]
         public string? Email { get; set; }
         public string? Professor { get; set; }
+
+        [Display(Name = "Início")]
         public DateTime? Data_Inicio { get; set; }
+
+        [Display(Name = "Fim")]
         public DateTime? Data_Fim { get; set; }
         public List<AvaliacaoViewModel>? LstAvaliacoes { get; set; }
         public List<AulaViewModel>? LstAulas { get; set; }
