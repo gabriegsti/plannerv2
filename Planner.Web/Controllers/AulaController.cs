c00d1f8f343cca9ac1e2ae29a2035f199396236d
gabriegsti <gabrielgsdevbr@gmail.com>
2022-06-24 15:15:44 -0300

    front de Aula funcionando

--- Planner.Web/Controllers/AulaController.cs
+++ Planner.Web/Controllers/AulaController.cs
@@ -8,10 +8,13 @@ namespace Planner.Web.Controllers
     public class AulaController : Controller
     {
         private readonly AulaRepositorio _repositorio;
+        private readonly MateriaRepositorio RepositorioMateria;
 
-        public AulaController(AulaRepositorio repositorio)
+        public AulaController(AulaRepositorio repositorio, MateriaRepositorio repositorioMateria)
         {
             _repositorio = repositorio;
+            RepositorioMateria = repositorioMateria;
+
         }
 
         public IActionResult Index()
@@ -35,6 +38,21 @@ namespace Planner.Web.Controllers
                 
         public IActionResult Cadastrar()
         {
+            List<MateriaViewModel> lst = new List<MateriaViewModel>();
+            IEnumerable<Materia> materias = RepositorioMateria.Buscar();
+
+            foreach (Materia materia in materias)
+            {
+                MateriaViewModel model = new MateriaViewModel();
+                model.Id_Materia = materia.Id_Materia;
+                model.Titulo = materia.Titulo;
+                model.Data_Inicio = materia.Data_Inicio;
+                model.Data_Fim = materia.Data_Fim;
+                model.Professor = materia.Professor;
+                model.Email = materia.Email;
+                lst.Add(model);
+            }
+            ViewBag.Materias = lst;
             return View();
         }
         [HttpPost]
@@ -45,7 +63,7 @@ namespace Planner.Web.Controllers
             aula.Data_Hora = aulaViewModel.Data_Hora;
             aula.Id_Aula = aulaViewModel.Id_Aula;
             aula.Link = aulaViewModel.Link;
-            aula.Titulo = aulaViewModel.Titulo.ToString();
+            aula.Id_Materia = aulaViewModel.Id_Materia;
 
             var id = _repositorio.Adicionar(aula);
 
@@ -72,5 +90,35 @@ namespace Planner.Web.Controllers
             _repositorio.Excluir(id);
             return RedirectToAction("Index");
         }
+
+        public IActionResult Editar(int id)
+        {
+            var aula = _repositorio.Buscar(id);
+            AulaViewModel model = new AulaViewModel();
+            model.Titulo = aula.Titulo;
+            model.Id_Materia = aula.Id_Materia;
+            model.Id_Aula = aula.Id_Aula;
+            model.Data_Hora = aula.Data_Hora;
+            model.Link = aula.Link;
+            var materia = RepositorioMateria.Buscar(aula.Id_Materia);
+            if(materia != null)
+                model.MateriaTitulo = materia.Titulo;
+            return View(model);
+        }
+
+        public IActionResult EditarPorId(AulaViewModel aulaViewModel)
+        {
+            Aula aula = new Aula();
+            aula.Id_Aula = aulaViewModel.Id_Aula;
+            aula.Titulo = aulaViewModel.Titulo;
+            aula.Id_Materia = aulaViewModel.Id_Materia;
+            aula.Data_Hora = aulaViewModel.Data_Hora;
+            aula.Link = aulaViewModel.Link;
+
+            _repositorio.Atualizar(aula);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
--- Planner.Web/Models/AulaViewModel.cs
+++ Planner.Web/Models/AulaViewModel.cs
@@ -8,7 +8,8 @@ namespace Planner.Web.Models
         [Key]
         [Required]
         public int Id_Aula { get; set; }
-        public int Id_materia { get; set; }
+        [Display(Name = "Matéria")]
+        public int Id_Materia { get; set; }
 
         [Required(ErrorMessage = "O campo titulo é obrigatório.")]
         public string Titulo { get; set; }
@@ -17,5 +18,7 @@ namespace Planner.Web.Models
         [Display(Name = "Dia/Horário")]
         public DateTime Data_Hora { get; set; }
         public string Link { get; set; }
+        [Display(Name = "Matéria")]
+        public string MateriaTitulo { get; set; }
     }
 }
