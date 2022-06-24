d2ca342aa18e91fb74db21d901bb0587232d6cc7
LuizAlberto93 <costaesilva93@outlook.com>
2022-06-24 18:45:15 -0300

    retirando nota

--- Planner.Web/Controllers/AvaliacaoController.cs
+++ Planner.Web/Controllers/AvaliacaoController.cs
@@ -38,25 +38,7 @@ namespace Planner.Web.Controllers
             return View(lst);
         }
 
-        public IActionResult Nota()
-        {
-            List<AvaliacaoViewModel> lst = new List<AvaliacaoViewModel>();
-            var avaliacoes = _repositorio.Buscar();
-
-            foreach (var avaliacao in avaliacoes)
-            {
-                AvaliacaoViewModel model = new AvaliacaoViewModel();
-                model.Id_Avaliacao = avaliacao.Id_Avaliacao;
-                model.Titulo = avaliacao.Titulo;
-                model.Data_Hora = avaliacao.Data_Hora;
-                model.Nota = avaliacao.Nota;
-                model.Titulo = avaliacao.Titulo.ToString();
-                lst.Add(model);
-            }
-
-            return View(lst);
-        }
-
+        
         public IActionResult Cadastrar()
         {
             List<MateriaViewModel> lst = new List<MateriaViewModel>();
